3779b724133cf18e8a88782ae2b1322c8f11b8b1
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2011-10-17 23:20:48 +0530

    Back-annotated DB tables onto model files...
    Command : bundle exec annotate --position before

--- webapp/app/models/question.rb
+++ webapp/app/models/question.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id         :integer         not null, primary key
+#  folder     :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Question < ActiveRecord::Base
 end
--- webapp/app/models/quiz.rb
+++ webapp/app/models/quiz.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: quizzes
+#
+#  id         :integer         not null, primary key
+#  teacher_id :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Quiz < ActiveRecord::Base
   belongs_to :teacher 
   has_many :questions 
