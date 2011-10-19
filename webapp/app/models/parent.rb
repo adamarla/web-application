c8a52e43694cbf95c89c85d017040debc5100834
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2011-10-19 16:16:13 +0530

    Reverting last change..
    Not sure whether it makes sense to delete students if parent is deleted.
    For one, I suspect that initially most student records will lack parent
    information. So, if student records can exist w/o parents at the time of
    creation, then why can't they for all future times ( or until we decide
    for some other reasons )

--- webapp/app/models/parent.rb
+++ webapp/app/models/parent.rb
@@ -9,7 +9,6 @@
 #
 
 class Parent < ActiveRecord::Base
-  # A parent is always a parent. If parent goes, so do the kids/students
-  has_many :students, :dependent => :destroy 
+  has_many :students
   has_one :account, :dependent => :destroy
 end
