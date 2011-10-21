4fe1763f99004f952f5c72b4c7bf1921a70e1d90
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2011-10-21 13:46:00 +0530

    Each student belongs to one study_group..

--- webapp/db/schema.rb
+++ webapp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111021074042) do
+ActiveRecord::Schema.define(:version => 20111021075751) do
 
   create_table "accounts", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(:version => 20111021074042) do
     t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "study_group_id"
   end
 
   create_table "study_groups", :force => true do |t|
