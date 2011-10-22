6f8928d4666d894aec434b3de6e6bd4459a66a3d
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2011-10-22 16:38:10 +0530

    Call private method set_uid after_create
    The in-memory object will not have a self.id till the time its not
    saved onto the database first. Hence, updating of self.uid = f(self.id..),
    makes sense only once self.id != null

--- webapp/app/models/quiz.rb
+++ webapp/app/models/quiz.rb
@@ -20,7 +20,7 @@ class Quiz < ActiveRecord::Base
             :numericality => {:only_integer => true, :greater_than => 0}, \
             :on => :create
 
-  before_create :set_uid
+  after_create :set_uid
 
   def prepare_for(students)
     # students : an array of selected students from the DB
@@ -33,13 +33,20 @@ class Quiz < ActiveRecord::Base
   private 
 
     def set_uid 
+      # Each quiz is given a unique ID. Within it is information about 
+      # the issuing teacher, # of students taking the quiz, index in DB etc. etc.
+      # which is really important at our end. 
+
+      # This UID is then mixed with student specific information to make the 
+      # student specific QR code. Note that this function can only be called 
+      # after_create because till then no self.id has been assigned
+
       a = [self.id, self.teacher_id, self.num_questions, self.num_students] 
+      uid = "" 
+
       a.each { |b|
-        if self.uid.nil? 
-            self.uid = "#{b.to_s(36).upcase}"
-        else 
-            self.uid += "#{b.to_s(36).upcase}"
-        end 
+        uid += "#{b.to_s(36).upcase}"
       }
+      self.update_attribute :uid, uid
     end 
 end
