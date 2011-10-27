00ffd77d91ea864f37c5c8486c67d713f83e968b
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2011-10-27 17:53:04 +0530

    Initial commit of code to provision a DB slot for a new question...
    The person adding a new question would not know what the last question
    in the DB is. Hence, what is needed is a way for the system to
    block a slot in the DB for the new question. Blocking a slot means
    not just adding a new entry into the DB, but also creating a new folder
    and adding that new folder into the Git repo

--- webapp/app/models/db_question.rb
+++ webapp/app/models/db_question.rb
@@ -15,7 +15,8 @@ class DbQuestion < ActiveRecord::Base
   
   # 'path' is relative to some root and should be of the form 'dir/dir/something'
   validates :path, :presence => true, 
-            :format => { :with => /\A([\/]?[-\w\d]+)*/, :message => "Should be a valid UNIX path" }
+            :format => { :with => /\A([\/]?[-\w\d]+)*/, 
+                         :message => "Should be a valid UNIX path" }
   
   has_many :questions # rather, instances of this question in various quizzes
   belongs_to :examiner
--- webapp/app/models/examiner.rb
+++ webapp/app/models/examiner.rb
@@ -11,4 +11,34 @@
 class Examiner < ActiveRecord::Base
   has_one :account 
   has_many :db_questions 
+
+  def provision_db_slot(attributes = {:subject => nil, :grade => nil, :flags => 0})
+    root = "#{ENV['VTA_ROOT']}/" 
+    # Puts checks either in the controller or in the javascript to ensure 
+    # that :subject and :grade are non-nil by the time they get here. Ideally
+    # :flags should also != 0. But :flags == 0 is something we can live with 
+    # till such time it is set
+    # Call : obj.provision_db_slot :subject => :maths, :grade => 9, :flags => 0022
+
+    subject = attributes[:subject] 
+    grade = attributes[:grade]
+
+    # 'rel_path' represents the path relative to VTA_ROOT where TeX for the new 
+    # question *will be* stored. However, the path is not fully known at the time of 
+    # creation. For one, we don't know what the last index in the DB is. Hence,
+    # we first block a slot and then store with a final value for 'rel_path'
+    rel_path = "#{subject.to_s}/G#{grade}"
+    new_dbq = self.db_questions.build(:path => rel_path, :flags => attributes[:flags])
+    new_dbq.save # there, a slot is now blocked in the DB
+
+    rel_path += "/#{new_dbq.id}"  
+    full_path = root + rel_path
+    new_dbq.update_attribute(:path,rel_path) # 2nd pass
+
+    # Now issue a series of system commands
+    system("mkdir -p #{full_path} && cp #{root}/common/Makefile #{full_path}")
+    system("cd #{root} && git add #{rel_path}")
+
+  end 
+
 end
