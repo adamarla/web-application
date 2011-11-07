6018268b85f5a96c3b534ab210affd99aacc99be
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2011-11-07 17:56:31 +0530

    Require authentication for all actions except 'insert_new'...
    'insert_new' is invoked by POST request sent by 'examiner' script.
    Admittedly, the ideal solution would be to authenticate user before
    this action too. But till such time that ability can be built in,
    I am bypassing authentication for it. AAnd anyways, 'insert_new'
    has its own basic 'secret-key' based authentication

--- webapp/app/controllers/question_controller.rb
+++ webapp/app/controllers/question_controller.rb
@@ -1,4 +1,6 @@
 class QuestionController < ApplicationController
+  before_filter :authenticate_account!, :except => [:insert_new]
+
   def insert_new
     # As of now, this action can be initiated only by the POST 
     # request sent by 'examiner' script. And the POST request sends 
