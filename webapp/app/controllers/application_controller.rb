abfb55a748fffd901258e73c0caee7f4543b48a0
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2011-11-15 19:33:56 +0530

    Redefined CanCan method current_ability to work our Account model..
    It seems that by default, CanCan assumes that the authentication
    model is called User. However, in our case, its called Account.
    Hence, we need to redefine an internal cancan method - current_ability -
    to make CanCan work with our model.
    
    Ref : https://github.com/ryanb/cancan/wiki/changing-defaults

--- webapp/app/controllers/application_controller.rb
+++ webapp/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private 
+    # [CanCan] : We have to redefine current_ability because our "user"
+    # model is not called User, but is instead called "Account" 
+    # Ref : https://github.com/ryanb/cancan/wiki/changing-defaults
+    def current_ability 
+      @current_ability ||= AccountAbility.new(current_account)
+    end 
 end
