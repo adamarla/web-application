5ee1707fa726332b1ca6800845687deab3cbef40
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2011-11-16 12:09:31 +0530

    Removed email column from Teacher..
    Email is already part of the Account model.. No need for it therefore
    in any of the other models

--- webapp/app/models/teacher.rb
+++ webapp/app/models/teacher.rb
@@ -5,7 +5,6 @@
 #  id         :integer         not null, primary key
 #  first_name :string(255)
 #  last_name  :string(255)
-#  email      :string(255)
 #  created_at :datetime
 #  updated_at :datetime
 #  school_id  :integer
--- webapp/db/schema.rb
+++ webapp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111113205359) do
+ActiveRecord::Schema.define(:version => 20111116063433) do
 
   create_table "accounts", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
@@ -190,7 +190,6 @@ ActiveRecord::Schema.define(:version => 20111113205359) do
   create_table "teachers", :force => true do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "school_id"
