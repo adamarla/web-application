53c2c8b42a10cee055eedd79faf5f13d5769206f
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2011-11-19 02:21:23 +0530

    Update action need only return HTTP headers with status code..

--- webapp/app/controllers/grade_descriptions_controller.rb
+++ webapp/app/controllers/grade_descriptions_controller.rb
@@ -7,13 +7,16 @@ class GradeDescriptionsController < ApplicationController
   def update
     # params => {:benchmarks => { 1 => {:annotation => .....}, .... } }
     benchmarks = params[:benchmarks] 
+    status = :ok 
+
     benchmarks.each { |id, attributes| 
       grade_desc = GradeDescription.find(id.to_i) 
       unless grade_desc.nil? 
-        grade_desc.update_attributes(attributes)
+        status = grade_desc.update_attributes(attributes) ? :ok : :bad_request
+        break if status == :bad_request
       end 
     }
-    redirect_to :back
+    head status # simply return HTTP headers with HTTP status code
   end
 
   def create
