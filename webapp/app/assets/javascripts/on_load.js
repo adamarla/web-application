6f2e167badb8fafcfa19e184174db557b93634b9
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2011-11-18 18:37:12 +0530

    Create button-sets for "resources" with multiple associated buttons...
    Note the use of the term "resources" and not "forms". A "resource"
    - as I intend it - is a logical grouping of HTML elements that provide
    some information to the user and allow certain actions to be performed
    on the underlying DB via it. A formtastic form made based on an ActiveRecord
    model is one example.
    
    Anyways, I think that in order to keep the HTMLs clean and self-consistent,
    its best to hand-code "submit" buttons ( rather than use f.buttons provided
    by formtastic ). Formtastic is great. But sometimes it creates too deeply
    nested a structure

--- webapp/app/assets/javascripts/on_load.js
+++ webapp/app/assets/javascripts/on_load.js
@@ -1,8 +1,8 @@
 
 $( function() { 
-	// Generate accordions 
-	$('.accordion-form').accordion({ header : '.accordion-heading', collapsible : true, active : false }) ;
+    // Generate accordions 
+    $('.accordion-form').accordion({ header : '.accordion-heading', collapsible : true, active : false }) ;
 
-	// Stylize formtastic 'submit' buttons 
-	$('input:submit').button() ;
+    
+    $('.submit-buttons').buttonset() ;
 }) ; 
