f069cef924cff0a1c910cd129fd19503e63e29ca
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2011-11-21 01:55:47 +0530

    Teachers can now edit their marking scheme...
    In other words, they can change the allotment for the various grades.
    They cannot, however, change the annotation nor the description of the
    grade's parent yardstick
    
    The same accordion - as in Admin console - is used. However, as mentioned
    above, the only editable field is 'allotment'. Rest all is text

--- webapp/app/controllers/grades_controller.rb
+++ webapp/app/controllers/grades_controller.rb
@@ -1,5 +1,20 @@
 class GradesController < ApplicationController
+
   def update
+    # params => {:grades => {1 => "60", 4 => "30", ....} .... }
+    grades = params[:grades] 
+    status = :ok 
+
+    grades.each { |id, allotment| 
+      grade = Grade.find(id) 
+      unless grade.nil? 
+        status = grade.update_attribute(:allotment, allotment) ? :ok : :bad_request
+      else
+        status = :bad_request
+      end 
+      break if status == :bad_request 
+    } 
+    head status 
   end
 
 end
