39466a4bfdb4b305b1c29119e704ece9e497482d
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2011-11-25 02:05:35 +0530

    Separated JS asset for admin from that for the overall application..
    This is the way it should be. Once you log in as a particular role,
    only the JS - and even the css - for that particular role should load.
    
    This requires creating separate assets for each role. Hence, admin.js -
    which was previously just another .js file - is now a manifest file.
    
    'application.js' is effectively empty for now and application layout
    simply yields. As far as I can tell, only WelcomeController would
    default to using application layout and assets. All other roles will
     - nay, should - have their own assets

---
+++ webapp/app/assets/javascripts/admin_on_load.js
@@ -0,0 +1,90 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+$( function() { 
+
+    // New Yardstick 
+    $('#new-yardstick-button').click( function() { 
+        $('#new-yardstick').dialog({
+            modal : true, 
+            title : "New Yardstick",
+            autoOpen : false 
+        }).dialog('open');
+    }) ;
+
+    // New School Board 
+    $('#new-school-board-link').click( function() {
+        $('#new-board-form').dialog({
+            modal : true, 
+            title : "New School Board", 
+            autoOpen : false
+        }).dialog('open') ;
+    }) ;
+
+    // New Course with the option of simultaneously adding some Courses
+    $('#new-course-link').click( function() {
+        var appendable = $('#new-board-form .appendable') ;
+        var next = parseInt($(appendable).attr('next')) + 1 ;
+        
+        // First, make a stub element
+        var clone = $('#new-course-form .extractable').clone() ; 
+        var stub = $('<div class="stub"></div>').prepend($(clone));
+
+        // Then, prepend the stub within appendable. Remember to increment i
+        // appendable's next counter 
+        $(stub).prependTo($(appendable)).hide().fadeIn('slow') ;
+        $(appendable).attr('next', next) ; 
+
+        // This is  really important. Set the 'name' attribute for <input> and 
+        // <select> within the newly added stub
+
+        $(stub).find('input,select').each( function() {
+            var name = $(this).attr('name') ;
+            // 'name' is of the form : courses[*][ grade | subject_id ... ]. The '*'
+            // has to be replaced with the current 'next'
+            name = name.replace(/\*/g, next) ; 
+            $(this).attr('name', name) ;
+        }) ;
+    }) ; // of function
+
+
+    // on-the-fly loading of Courses for a selected Board 
+    $('#board-list input[type="radio"]').click( function() { 
+      var marker = $(this).attr('marker') ; 
+      var url = "/get_course_details/" + marker ;
+
+      $.get(url, function(data){
+        var courses = data.board.courses ; 
+
+        $('#courses-table .data:first').empty() ; // empty old table content
+
+        $.each( courses, function(index, hash) { 
+           var course = hash.course ; 
+           var cells = [] ; 
+
+           // Push a triplet where : 
+           //   1st = display text
+           //   2nd = class attribute to set on table cell
+           //   3rd = some other relevant, non-display data
+           cells.push(["edit", "quick-link", course.id]) ;
+           cells.push([course.name, "wide", null]) ; 
+           cells.push([course.subject.name, "regular", null]) ; 
+           cells.push([course.grade, "narrow", null]) ; 
+
+           // alert(" Course ID = " + course.id) ;
+           var tableRow = createTableRow( cells ) ;
+
+           tableRow.addClass('greedy') ;
+           if (index % 2 == 1) { 
+             $(tableRow).addClass('color') ;
+           } 
+
+           // Now insert the row inside $('#courses-table') 
+           $('#courses-table .data:first').append( tableRow ).hide().fadeIn('slow') ;
+
+        }) ; 
+      }, "json") ;
+    }) ;
+
+
+}) 
--- webapp/app/assets/javascripts/application.js
+++ webapp/app/assets/javascripts/application.js
@@ -1,10 +1,3 @@
 // The recommended practice is not to include any JS code 
 // in a manifest file - like this one. Instead, split JS code 
 // in other .js files and require them as needed
-
-//= require jquery 
-//= require jquery_ujs 
-//= require jquery-ui
-//= require utility
-//= require common_bindings
-//= require admin
