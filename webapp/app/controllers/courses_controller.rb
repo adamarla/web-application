0ea08a35a03b0c054193ac71068bca8e58ec898b
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2011-11-25 16:08:27 +0530

    Added shells for show and update actions.. More to follow

--- webapp/app/controllers/courses_controller.rb
+++ webapp/app/controllers/courses_controller.rb
@@ -1,2 +1,12 @@
 class CoursesController < ApplicationController
+  respond_to :json 
+
+  def show 
+    @course = Course.find(params[:id]) 
+    respond_with @course 
+  end 
+
+  def update 
+    head :ok 
+  end 
 end
