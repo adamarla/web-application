552a2589c5829d2b813f186629eb83d6ac4f0866
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2011-11-27 13:18:14 +0530

    Added form and controller action to add a new a school to the DB

--- webapp/app/controllers/schools_controller.rb
+++ webapp/app/controllers/schools_controller.rb
@@ -2,12 +2,28 @@ class SchoolsController < ApplicationController
   respond_to :json 
 
   def create 
-    head :ok
+    email = params[:school].delete :email # email is part of Account model 
+
+    unless (email.nil? || email.empty?) 
+      school = School.new params[:school]
+      zip = school.zip_code 
+      status = :ok 
+
+      unless zip.nil? 
+        school.build_account :email => email, :password => zip, :password_confirmation => zip 
+        status = school.save ? :ok : :bad
+      else
+        status = :bad 
+      end 
+    else 
+      status = :bad
+    end 
+    head status 
   end 
 
   def list 
     @schools = School.all 
-	respond_with @schools 
+    respond_with @schools 
   end 
 
 end
