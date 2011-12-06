3d9c09aef6c3d2efb069863713a7311e9e8e8a72
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2011-12-06 05:41:44 +0530

    Preparing to remove utility/shared.js and the whole utility/ folder ...
    the old scheme of a shared folder was better

--- webapp/app/assets/javascripts/shared/utility.js
+++ webapp/app/assets/javascripts/shared/utility.js
@@ -1,4 +1,18 @@
 
+/*
+Copy element X to Y. This needs to be done when - for example - one 
+needs to make a search box by picking elements from the toolbox
+*/ 
+
+function copyXtoY(X,Y) { // X,Y are CSS-selectors
+  var source = $(X) ; 
+  var target = $(Y) ;
+
+  if (source.length == 0 || target.length == 0) return false ;
+  source.clone().appendTo(target) ;
+  return true ;
+} 
+
 function editFormAction(formId, url, method) {
   // JS has no native support for default arguments. So, this is what one does
   method = (typeof method == 'undefined') ? 'post' : method ;
--- webapp/app/assets/javascripts/utility/shared.js
+++ webapp/app/assets/javascripts/utility/shared.js
@@ -1,15 +1,1 @@
 
-/*
-Copy element X to Y. This needs to be done when - for example - one 
-needs to make a search box by picking elements from the toolbox
-*/ 
-
-function copyXtoY(X,Y) { // X,Y are CSS-selectors
-  var source = $(X) ; 
-  var target = $(Y) ;
-
-  if (source.length == 0 || target.length == 0) return false ;
-  source.clone().appendTo(target) ;
-  return true ;
-} 
-
