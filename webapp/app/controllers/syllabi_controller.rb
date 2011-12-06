2999e98a37430b2977844e095bb809ff6e316518
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2011-12-06 07:59:29 +0530

    Inserted a trojan horse for course_id in edit-syllabus-form

--- webapp/app/controllers/syllabi_controller.rb
+++ webapp/app/controllers/syllabi_controller.rb
@@ -3,7 +3,8 @@ class SyllabiController < ApplicationController
 
   def update 
     options = params[:syllabi] 
-    course_id = options.nil? ? nil : options.delete(:course_id)
+    #course_id = options.nil? ? nil : options.delete(:course_id)
+    course_id = params[:course_id].nil? ? nil : params[:course_id][:marker] 
     course = course_id.nil? ? nil : Course.find(course_id) 
 
     status = course.nil? ? :bad_request : course.update_syllabus(params[:syllabi]) 
