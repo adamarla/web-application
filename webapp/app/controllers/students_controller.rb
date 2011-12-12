a03d053818d106c8410c51d2859a1c3e488129fe
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2011-12-12 15:01:35 +0530

    Student create action should also return some JSON
    .. like Teacher create action does. The returned JSON could
    be used to update display at client end

--- webapp/app/controllers/students_controller.rb
+++ webapp/app/controllers/students_controller.rb
@@ -4,21 +4,20 @@ class StudentsController < ApplicationController
   def create 
     school_id = params[:student].delete :marker 
     school = School.find school_id 
-    status = school.nil? ? :bad_request : :ok 
 
-    head status if status == :bad_request 
+    head :bad_request if school.nil?
     
-    student = Student.new params[:student]
-    username = student.generate_username
+    @student = Student.new params[:student]
+    username = @student.generate_username
     email = params[:student].delete(:email) || "#{username}@drona.com"
-    student.school = school 
+    @student.school = school 
     password = school.zip_code
 
     unless username.blank?
-      account = student.build_account :username => username, :email => email,  
+      account = @student.build_account :username => username, :email => email,  
                                       :password => password, :password_confirmation => password
     end 
-    head (student.save ? :ok : :bad_request) 
+    @student.save ? respond_with(@student) : head(:bad_request)  
   end 
 
 end
