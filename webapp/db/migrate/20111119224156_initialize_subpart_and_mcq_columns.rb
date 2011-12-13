9212ca576b67c1510dc8e9dc9fc03ba186ef94be
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2011-12-13 06:52:46 +0530

    [postgres] : Corrected uninitialized constant error during migration ..
    Ok, I am not quite sure why the change worked. But it worked.
    And why isn't this change required in every subsequent migration with
    reset_column_information? Don't know .. Time for StackOverflow
    
    Ref : http://blazingcloud.net/2010/12/02/heroku-migration-fails-to-update-attributes-reset_column_information/

--- webapp/db/migrate/20111119224156_initialize_subpart_and_mcq_columns.rb
+++ webapp/db/migrate/20111119224156_initialize_subpart_and_mcq_columns.rb
@@ -1,4 +1,5 @@
 class InitializeSubpartAndMcqColumns < ActiveRecord::Migration
+  class GradeDescription < ActiveRecord::Base ; end 
   def change 
     GradeDescription.reset_column_information 
     GradeDescription.all.each { |g| g.update_attributes :mcq => false, :subpart => false }
