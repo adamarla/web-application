791bda07030d161f9b16963146a87c9ddacc62ec
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2011-12-27 11:12:24 +0530

    Removed redundant code

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -51,15 +51,4 @@ class CoursesController < ApplicationController
     @macros = MacroTopic.where('id IS NOT NULL').order(:name) # basically, everyone
   end 
 
-=begin
-  def load 
-    @course = Course.find params[:id] 
-    unless @course.nil? 
-      respond_with @course 
-    else 
-      head :bad_request 
-    end 
-  end 
-=end
-
 end
