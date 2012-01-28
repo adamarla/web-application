fbb902d91fd7fce3f41a02f7eb9d18ec6942e6e8
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2012-01-29 02:02:01 +0530

    YAML generated global hash to store Gutenberg specific settings

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Webapp::Application.initialize!
+
+
+# Using YAML to set application-wide variables - Railscast #85
+Gutenberg = YAML.load_file("#{Dir.pwd}/config/gutenberg.yml")
