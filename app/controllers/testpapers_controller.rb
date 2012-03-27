7a6a1ea51c313c5c67dfb5d7efe6f2a9409dfc51
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2012-03-28 03:02:03 +0530

    Sending back information about whether or not a testpaper has been graded for a student ..
    if not, then it should show as 'pending' in the teacher's report-card panel

--- app/controllers/testpapers_controller.rb
+++ app/controllers/testpapers_controller.rb
@@ -1,10 +1,14 @@
 class TestpapersController < ApplicationController
+  before_filter :authenticate_account!
+  respond_to :json
 
   def students
     # students who got this testpaper
-    testpaper = Testpaper.find params[:id]
-    head :bad_request if testpaper.nil?
-    @students = testpaper.students
+    @testpaper = Testpaper.find params[:id]
+    head :bad_request if @testpaper.nil?
+    @students = @testpaper.students
+
+    respond_with @students, @testpaper
   end
 
 end
--- app/views/testpapers/students.json.rabl
+++ app/views/testpapers/students.json.rabl
@@ -1,3 +1,8 @@
 
 collection @students => :students 
   attributes :name, :id
+
+  code :graded do |s|
+    c = CoursePack.where(:student_id => s.id, :testpaper_id => @testpaper.id).first
+    c.graded?
+  end
