6b985d23a045b7ee7230a023b78edfbe0746a929
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2012-03-29 02:21:25 +0530

    List students who have taken a Quiz alphabetically by first name

--- app/controllers/testpapers_controller.rb
+++ app/controllers/testpapers_controller.rb
@@ -6,7 +6,7 @@ class TestpapersController < ApplicationController
     # students who got this testpaper
     @testpaper = Testpaper.find params[:id]
     head :bad_request if @testpaper.nil?
-    @students = @testpaper.students
+    @students = @testpaper.students.order(:first_name)
 
     respond_with @students, @testpaper
   end
