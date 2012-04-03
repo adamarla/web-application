01397c20a04e2c292376a0a5e5fef6ee3542be2f
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2012-04-04 01:10:42 +0530

    Reverting to when it all worked ..
    But support for IE < 8 is still a massively important to-do

--- app/assets/javascripts/teachers.js
+++ app/assets/javascripts/teachers.js
@@ -2,4 +2,3 @@
 //= require_directory ./teachers 
 //= require ../../../vendor/assets/javascripts/flot/jquery.flot.min
 //= require ../../../vendor/assets/javascripts/flot/jquery.flot.threshold.min
-//= require ../../../vendor/assets/javascripts/flot/excanvas.min
