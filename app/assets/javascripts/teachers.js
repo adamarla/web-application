7706b57033c22f3924b58203e3f86199453877a7
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2012-04-04 01:00:55 +0530

    Yet another stab at excanvas inclusion

--- app/assets/javascripts/teachers.js
+++ app/assets/javascripts/teachers.js
@@ -2,3 +2,4 @@
 //= require_directory ./teachers 
 //= require ../../../vendor/assets/javascripts/flot/jquery.flot.min
 //= require ../../../vendor/assets/javascripts/flot/jquery.flot.threshold.min
+//= require ../../../vendor/assets/javascripts/flot/excanvas.min
