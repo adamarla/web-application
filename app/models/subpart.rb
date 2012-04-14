892083194a18a97e5fdc2afabc91bfc5df4463c4
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2012-04-15 04:07:54 +0530

    Defined method to return name for a subpart ..
    The returned name is a function of the quiz in which the subpart's
    parent question is instantiated

--- app/models/subpart.rb
+++ app/models/subpart.rb
@@ -15,4 +15,20 @@
 class Subpart < ActiveRecord::Base
   belongs_to :question
   has_many :graded_responses
+
+  def name?(quiz_id)
+    # What a subpart is called depends on the quiz in which the parent 
+    # question is instantiated. The same question could be Q2 in one 
+    # quiz and Q7 in another
+    parent = self.question
+    id = QSelection.where(:quiz_id => quiz_id, :question_id => parent.id).map(&:index).first
+    nparts = parent.num_parts?
+    if nparts == 0
+      return "Ques. #{id}"
+    else
+      c = [*'A'..'K'][self.index]
+      return "Ques. #{id}#{c}"
+    end
+  end
+
 end
