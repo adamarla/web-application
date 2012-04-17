993833da681c161706c9c9f277cd1e0d42c6a371
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2012-04-17 23:23:31 +0530

    New query? method to tell whether or a given subpart is on given pages in given quiz

--- app/models/subpart.rb
+++ app/models/subpart.rb
@@ -16,4 +16,24 @@ class Subpart < ActiveRecord::Base
   belongs_to :question
   has_many :graded_responses
 
+  def on_page? (n,quiz)
+    # Returns true if this subpart is on page 'n' of 'quiz'
+    # 'n' can be an array of page numbers too, like [4,7,9] - in which case
+    # the method would return true if the subpart is on either of the pages
+    # in the given quiz
+
+    s = QSelection.where(:question_id => self.question_id, :quiz_id => quiz)
+    return false if s.empty? 
+
+    start_pg = s.select(:start_page).first.start_page
+    pg = start_pg + self.offset
+
+    if n.class == Array
+      return n.include? pg
+    else 
+      return pg == n
+    end
+
+  end # of method
+
 end
