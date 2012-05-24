b6cc4b13009536d376cb1acc24a865f4921cef8e
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2012-05-24 11:08:46 +0530

    No need for a welcome.js

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,5 +1,3 @@
 // The recommended practice is not to include any JS code 
 // in a manifest file - like this one. Instead, split JS code 
 // in other .js files and require them as needed
-
-//= require_directory ./welcome 
