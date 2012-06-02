22dc5093eb52b483ca06c18ae18da4c7987eebec
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2012-06-02 08:52:11 +0530

    Annotation added

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,2 +1,11 @@
+# == Schema Information
+#
+# Table name: countries
+#
+#  id           :integer         not null, primary key
+#  name         :string(255)
+#  alpha_2_code :string(255)
+#
+
 class Country < ActiveRecord::Base
 end
