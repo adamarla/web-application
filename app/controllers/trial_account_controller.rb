1355deff542d627a8f528001e1f4ee409c6e5525
Akshay Damarla <akshay.damarla@gmail.com>
2012-06-07 17:05:03 +0530

    - cosmetic change to success confirmation page for trial account creation - added some @instance variables to the controller for display in view

--- app/controllers/trial_account_controller.rb
+++ app/controllers/trial_account_controller.rb
@@ -9,15 +9,15 @@ class TrialAccountController < ApplicationController
     unless school.nil?
       @teacher = Teacher.new :name => p[:name], :school_id => school.id
       username = create_username_for @teacher, :teacher 
-      email = p[:email] 
-      password = p[:zip_code]
-      account = @teacher.build_account :email => email, :username => username,
-                :password => password, :password_confirmation => password
+      @email = p[:email] 
+      @password = p[:zip_code]
+      account = @teacher.build_account :email => @email, :username => username,
+                :password => @password, :password_confirmation => @password
 
       maths = Subject.where(:name => "Maths").map(&:id)
       @teacher.set_subjects maths
       @teacher.sektions = school.sektions
-
+	  
       trial = @teacher.build_trial_account :school => p[:school], 
                 :zip_code => p[:zip_code], :country => p[:country].to_i
       if @teacher.save
