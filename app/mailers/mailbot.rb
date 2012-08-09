47980223d2991859259514ad6b830b959cd0243d
Akshay Damarla <akshay.damarla@gmail.com>
2012-08-09 10:31:56 +0530

    - bug in setting up email message

--- app/mailers/mailbot.rb
+++ app/mailers/mailbot.rb
@@ -33,7 +33,7 @@ class Mailbot < ActionMailer::Base
   def suggestion_typeset( suggestion )
     t = Teacher.where(:id => suggestion.teacher_id).first 
     @name = t.first_name 
-    @date = suggestion.created_on 
+    @date = suggestion.created_at 
     mail :to => t.account.email, :subject => "Your questions have been typeset"
   end
 
