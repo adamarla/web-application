8d6158afbb2460a189c46265bce4e372639164f3
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2012-08-26 10:46:27 +0530

    [Bug]: Calibrations should have been created only for the new MCQ yardsticks - not all

--- db/migrate/20120824102710_define_new_calibrations.rb
+++ db/migrate/20120824102710_define_new_calibrations.rb
@@ -3,8 +3,9 @@ class DefineNewCalibrations < ActiveRecord::Migration
     # non-mcq / subjective calibrations 
     Calibration.build_viable_calibrations
 
-    # mcq calibrations 
-    Yardstick.mcqs.each do |m|
+    # mcq calibrations based on 'new' yardsticks only. The old ones have 
+    # IDs < 13
+    Yardstick.mcqs.where('id > ?', 13).each do |m|
       v = Calibration.fair_value_for m
       c = Calibration.new :mcq_id => m.id, :allotment => v
       c.save
