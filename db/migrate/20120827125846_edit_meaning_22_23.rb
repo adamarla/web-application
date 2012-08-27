d4f99639c72d88202ae8c8466c1cd4a3493f1cbc
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2012-08-27 20:19:26 +0530

    Minor edits to meaning

--- db/migrate/20120827125846_edit_meaning_22_23.rb
+++ db/migrate/20120827125846_edit_meaning_22_23.rb
@@ -3,7 +3,7 @@ class EditMeaning2223 < ActiveRecord::Migration
     Yardstick.find(22).update_attribute :meaning, 
         "Follow-up work is more incomplete than complete OR has some errors"
     Yardstick.find(23).update_attribute :meaning,
-        "An honest attempt with no serious errors - but with some loose ends"
+        "An honest attempt with no serious errors - but only partially complete. Not all loose ends tied up"
   end
 
   def down
