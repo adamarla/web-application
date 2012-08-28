97b779f89edf39549006b7d5ec23faa87b796d24
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2012-08-28 20:44:28 +0530

    Disable calibrations - but dont actually destroy them - if destroy is called on them

--- app/models/calibration.rb
+++ app/models/calibration.rb
@@ -223,6 +223,8 @@ class Calibration < ActiveRecord::Base
     end 
 
     def destroyable?
+      # De-activate the calibration - but don't actually destroy it
+      self.update_attribute :enabled, false
       return false
     end
 
