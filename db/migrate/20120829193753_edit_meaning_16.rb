b9208f24b058ba1e477d11f3f55ea6789fe87811
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2012-08-31 16:08:39 +0530

    Separate yardstick for when a critical insight is missing

--- db/migrate/20120829193753_edit_meaning_16.rb
+++ db/migrate/20120829193753_edit_meaning_16.rb
@@ -1,7 +1,7 @@
 class EditMeaning16 < ActiveRecord::Migration
   def up
     Yardstick.find(16).update_attribute :meaning, 
-    %q(And the approach taken is either very inefficient or will not lead to the correct answer)
+    %q(And the approach taken is inefficient. Getting to the right answer unlikely even if given a little more time) 
   end
 
   def down
