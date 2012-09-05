e9164bc7bed2a7578dbb90161b31da97a1d0159e
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2012-09-05 14:51:50 +0530

    Last commit should have been done in 2 steps ..
    The migration assumed a certain DB schema. But because all the changes were
    pushed one go, the schema moved forward and therefore the migration went
    out-of-sync
    
    Trying to correct that with this commit

--- db/migrate/20120905071229_no_grade_ids.rb
+++ db/migrate/20120905071229_no_grade_ids.rb
@@ -1,11 +1,13 @@
 class NoGradeIds < ActiveRecord::Migration
   # This is an irreversible migration
   def up 
-    GradedResponse.graded.each do |m|
-      c = m.grade.calibration_id
-      m.update_attribute :grade_id, c
-    end 
     rename_column :graded_responses, :grade_id, :calibration_id
+    # At this time, the calibration_id actually has the old grade_id
+    GradedResponse.graded.each do |m|
+      g = Grade.where(:id => m.calibration_id).first
+      c = g.nil? ? nil : g.calibration_id 
+      m.update_attribute :calibration_id, c 
+    end 
   end
 
   def down
