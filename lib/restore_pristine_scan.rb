1f2309618fdce9a5965c9ad73da7f8e31fb28506
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2012-12-04 12:23:51 +0530

    Reset graded responses on a scan if that scan is restored to pristine state

--- lib/restore_pristine_scan.rb
+++ lib/restore_pristine_scan.rb
@@ -1,6 +1,15 @@
 
 class RestorePristineScan < Struct.new(:scan_locker_path)
   def perform
+    # Reset the graded responses corresponding this scan 
+    scan = scan_locker_path.split('/').last
+    scan = scan.length == 2 ? scan.last : nil 
+    unless scan.blank?
+      GradedResponse.where(:scan => scan).each do |m| 
+        m.reset
+      end 
+    end
+    # Then, issue request for restoring the pristine copy of the scan 
     SavonClient.http.headers["SOAPAction"] = "#{Gutenberg['action']['undo_annotate']}" 
     response = SavonClient.request :wsdl, :undoAnnotate do  
       soap.body = {
