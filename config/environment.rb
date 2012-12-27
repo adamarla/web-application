20264d97891e3582707b8f7ac89f7ac6d129b96f
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2012-12-27 14:01:21 +0530

    Set the starting indices of the first item under various rubric heads ..
    This info will help speed up lookup and rubric-based calculations

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,22 @@ Webapp::Application.initialize!
 # Using YAML to set application-wide variables - Railscast #85
 Gutenberg = YAML.load_file("#{Dir.pwd}/config/gutenberg.yml")[Rails.env]
 OnClick = YAML.load_file("#{Dir.pwd}/config/onclick.yml")
+
 Rubric = YAML.load_file("#{Dir.pwd}/config/rubric.yml")['rubric']['maths']
+# Set the start-indices of the first item under various heads
+['cogent', 'complete', 'other'].each do |m| 
+  case m
+    when 'cogent'
+      prev = ['honest']
+    when 'complete'
+      prev = ['honest', 'cogent']
+    when 'other'
+      prev = ['honest', 'cogent', 'complete']
+  end
+  n = 0 
+  prev.each { |m| n += Rubric[m].length } 
+  Rubric['start'][m] = n
+end 
 
 SavonClient = Savon::Client.new do
   wsdl.document = "#{Gutenberg['wsdl']}"
