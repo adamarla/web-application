49bfd121965a87866d1816db47fa11ee6ace4f67
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2012-12-27 09:51:46 +0530

    utility methods to query DOM built by rubric.yml

--- lib/general_queries.rb
+++ lib/general_queries.rb
@@ -21,4 +21,33 @@ module GeneralQueries
     n_pgs = (n_entries / per_pg.to_f).ceil
     return per_pg, n_pgs
   end
+
+  def mangle_feedback(hash) 
+    # 'hash' is params[:checked] submitted via a form by the grader
+    # Returns: An integer that should be stored as feedback for the graded response
+  end
+
+  def unmangle_feedback(n)
+    # 'n' is the 32-bit number stored as feedback for a graded response in the DB
+    # Returns: (overall, cogency, completeness, [ other issues ])
+  end
+
+  def quantify_feedback(n)
+    # Returns: Percentage of marks to give based on stored feedback 
+  end
+
+  def rubric_first_of_x_at(type)
+    # Returns the 0-indexed relative position of the * first * item listed under heading 'type'
+    # Assumes relative position of 'honest', 'cogent', 'complete' and 'other' is maintained in YAML
+    case type 
+      when 'honest' then prev = []
+      when 'cogent' then prev = ['honest']
+      when 'complete' then prev = ['honest', 'cogent']
+      when 'other' then prev = ['honest', 'cogent', 'complete']
+    end
+    ret = 0 
+    prev.each { |m| ret += Rubric[m].length }
+    return ret > 0 ? ret - 1 : 0
+  end
+
 end
