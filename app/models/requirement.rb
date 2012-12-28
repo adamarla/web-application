a1daaa571761cf0acc658a83f1f34f8bcf96259d
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2012-12-28 11:45:48 +0530

    Two important class methods added ...
    1. mangles params[:checked] hash into an integer. The integer is stored as feedback
       in the graded response
    2. Un-mangles this stored integer into an array of Requirement indices

--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -17,9 +17,81 @@ class Requirement < ActiveRecord::Base
 
   before_save :ensure_unique_type
 
+  def self.mangle_into_feedback( hash )
+    # hash = params[:checked]
+    ids = hash.keys.map(&:to_i).sort
+    mangled = 0 
+    n_other = 0
+
+    ids.each do |m|
+      type, index = Requirement.get_type_and_rel_index m
+      shift = 0
+      # Each requirement can have 15 scales => index = [0,14]. That looks plenty enough 
+      # But if you find yourself adding a lot of new scales, then buttress the code here
+      case type 
+        when :honest 
+          shift = 0     
+        when :cogent 
+          shift = 4
+        when :complete
+          shift = 8
+        when :other
+          shift = n_other < 4 ? 12 + (n_other << 2) : -1 # => atmost *5* other requirements
+          n_other += 1
+      end # of case  
+      break if shift < 0 
+      mangled |= (index << shift)
+    end # of each 
+    return mangled
+  end
+
+  def self.unmangle_feedback(n)
+    # Take the stored integer and return the list of Requirement IDs
+    # Returns : [a,b,c,d,e,f ..], where its understood that a => honest, b => cogent, c => complete, d .. => other
+    mask = 15
+    rel = []
+
+    [0,4,8,12,16,20,24,28].each do |shift|
+      m = ( n & (mask << shift)) >> shift 
+      next if m == 0
+      rel.push (m-1)
+    end
+
+    actual = [] 
+    rel.each_with_index do |i,j|
+      case j
+        when 0
+          r = Requirement.where(:honest => true)
+        when 1
+          r = Requirement.where(:cogent => true)
+        when 2
+          r = Requirement.where(:complete => true)
+        else
+          r = Requirement.where(:other => true)
+      end # of case
+      r = r.order(:id).map(&:id)
+      actual.push r[i]
+    end # of each
+    return actual
+  end
+
   private 
     def ensure_unique_type
       return [self.honest, self.cogent, self.complete, self.other].count(true) == 1
     end 
+
+    def self.get_type_and_rel_index(obj)
+      obj = obj.class == Fixnum ? Requirement.find(obj) : obj
+      type = obj.other ? :other : (obj.honest ? :honest : (obj.cogent ? :cogent : :complete))
+      r = nil
+      case type
+        when :other then r = Requirement.where(:other =>  true)
+        when :honest then r = Requirement.where(:honest =>  true)
+        when :cogent then r = Requirement.where(:cogent =>  true)
+        when :complete then r = Requirement.where(:complete =>  true)
+      end
+      r = r.order(:id).map(&:id)
+      return [type, r.index(obj.id) + 1] # 1-indexed to diff. b/w no entry and first entry
+    end
 
 end
