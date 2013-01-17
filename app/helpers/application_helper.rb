dabc933e91b658aa20ecedf2d9005f93408468d0
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2013-01-17 19:20:12 +0530

    Redundant code purged

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -114,67 +114,6 @@ module ApplicationHelper
     content_tag(:div, x) 
   end 
 
-  def trojan_horse_for(name) 
-    # creates a hidden <input> field inside a form, pre-populated - 
-    # usually by the 'marker' attribute of the containing DOM element
-    #   <input type="text" class="hidden" name="student[marker]"
-
-    tag :input, :type => :text, :class => :hidden, :name => "#{name.to_s}[marker]", :trojan => true
-  end 
-
-  def make_link (type, options = {})
-    # 'dynamic' links are those that load tables and controls on click. Non-dynamic links, 
-    # like for logout and login, don't
-
-    name = options[:for].to_s 
-    href = options[:href] || '#' 
-    with = options[:with] 
-    default = options[:default].nil? ? false : options[:default]
-    label = options[:as] || name
-    block_ajax = options[:block_ajax] || nil
-    select_sth = options[:select_sth] || nil
-
-    if type == :main
-      singular = name.singularize
-      plural = name.pluralize
-      dynamic = options[:dynamic].nil? ? true : options[:dynamic] 
-      class_attr = 'main-link'
-
-      if dynamic
-         # for main-links, the side-panel is fixed. Anything else specified 
-         # using :with is ignored   
-
-         panels = with.blank? ? {} : with[:panels] 
-         panels = panels.merge( { :side => "##{plural}-summary" } )
-         controls = (with.blank? || with[:controls].blank?) ? "##{singular}-controls" : with[:controls]
-      else 
-         panels = {} 
-         controls = nil 
-      end 
-    elsif type == :minor 
-      class_attr = 'minor-link'
-      panels = with.blank? ? {} : with.delete(:panels) 
-      controls = nil # minor-links cannot load any controls
-    end 
-
-    
-    link = link_to label.humanize, href, :id => "#{name}-link", :class => class_attr, 
-                               :side => panels[:side], :middle => panels[:middle], 
-                               :right => panels[:right], :wide => panels[:wide], 
-                               'load-controls' => controls, :default => default,
-                               :block_ajax => block_ajax, :select_sth => select_sth
-
-    return content_tag(:li, link, :id => "#{name}-anchor") 
-  end 
-
-  def main_link(options = {}) 
-    make_link :main, options 
-  end 
-
-  def minor_link(options = {}) 
-    make_link :minor, options 
-  end 
-
 =begin
    Usage Example : drop_down_menu_for :subjects, :disabled => true, :name => 'subjects[1]'
    Options : 
@@ -183,56 +122,6 @@ module ApplicationHelper
          :include_blank => [true | false]
          :slider => [true | false] (Sets attribute 'slider=true' attribute on the dropdown) 
 =end 
-
-  def drop_down_menu_for (sth, options = {}) 
-    collection = nil 
-
-    plural = (sth == :difficulty) ? sth : sth.to_s.pluralize.to_sym
-    singular = plural.to_s.singularize
-
-    disabled = options[:disabled].nil? ? false : options[:disabled]
-    include_blank = options[:include_blank].nil? ? true : options[:include_blank]
-    prefix = options[:name].nil? ? :criterion : options[:name]
-    slider = options[:slider].nil? ? false : ((plural == :percentages) && options[:slider]) 
-
-    case plural
-      when :boards 
-        collection = Board.all 
-      when :klasses 
-        collection = [*9..12] 
-      when :difficulty 
-        collection = {'junior school(<9)' => 1, 'middle(9-10)' => 2, 'senior(11-12)' => 3}
-      when :subjects 
-        collection = Subject.all
-      when :states 
-        collection = ['DL','HR','PB','MH','WB','UP','TN']
-      when :percentages 
-        collection = 0.step(101,5).to_a 
-    end 
-
-    unless collection.nil? || collection.empty?
-      select_box = semantic_fields_for prefix do |a| 
-                     a.input singular, :as => :select, :collection => collection, 
-                     :include_blank => include_blank,
-                     :input_html => { :disabled => disabled,
-                                      :slider => slider }
-                   end 
-    end # submitted by params as : criterion => {:state => "MH", :difficulty => "2"}
-
-    case options[:float]
-      when :right then class_attr = 'right dropdown'
-      when :none then class_attr = 'dropdown'
-      else class_attr = 'left dropdown'
-    end
-
-    if options[:id].nil? 
-      content_tag(:div, select_box, :id => "#{singular}-dropdown", 
-                  :class => class_attr)
-    else 
-      content_tag(:div, select_box, :id => "#{singular}-dropdown", 
-                  :class => class_attr, :marker => "#{id}")
-    end 
-  end # of helper  
 
   def nofrills_checkbox(options = {}) 
     name = options[:name].nil? ? nil : options[:name]
