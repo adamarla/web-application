e26ffbb65c5193d2dcdcb3d2c8e70b4f271a6f6b
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2013-01-22 13:55:51 +0530

    Force syck as YAML parser in all modes - especially deelopment

--- config/boot.rb
+++ config/boot.rb
@@ -4,3 +4,9 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+# Force use of old YAML parser - syck - in all modes. Locally, in dev mode, 
+# Ruby forces use of the new parser - psych. But in production, Heroku defaults 
+# syck 
+require 'yaml'
+YAML::ENGINE::yamler = 'syck'
