a4ee53f148aecd2bbdb049eaa35bb6be3b58ca2e
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2013-02-12 19:04:44 +0530

    Allow reg-exp for top-level keys
    Useful when - for example - one needs to create similar links
    with similar names using a for loop in the HAML
    
    Rather than list individual names in the YAML, one could simply
    write a regular a regular expression so that all the links match
    and get the same properties

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,6 +18,16 @@ module ApplicationHelper
 
     unless id.blank?
       onclick = OnClick[id]
+
+      # Allow keys that are actually reg-exps. Useful when a whole family of similarly named 
+      # keys need to be generated via a for loop in the HAML
+      if onclick.nil?
+        keys = OnClick.keys
+        matches = keys.map{ |k| /#{k}/.match("#{id}") } # k could be a reg-exp
+        idx = matches.index{ |m| m }
+        onclick = idx.nil? ? nil : OnClick[keys[idx]] # first place where reg-ex match passes
+      end
+
       unless onclick.nil?
         first_level_keys = [
           :left,
