ab21c7e7204d49ee8922da4ef733e4e92229eb60
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2013-04-03 15:06:57 +0530

    Added methods to encrypt / decrypt database IDs
    Testpaper and student IDs will be encrypted to form the QR code
    
    They will be decrypted during receive_scans and unresolved resolution

--- lib/general_queries.rb
+++ lib/general_queries.rb
@@ -1,5 +1,8 @@
 
 module GeneralQueries
+  BIG_7 = (36 ** 7) - 1
+  BIG_3 = (36 ** 3) - 1
+
   def pagination_layout_details(n_entries)
 =begin
     Kaminari offers helpful helper methods to generate the correct # of 
@@ -20,6 +23,44 @@ module GeneralQueries
     per_pg = (n_entries / default_per_pg.to_f).ceil > max_pages ? (n_entries / max_pages.to_f).ceil : default_per_pg 
     n_pgs = (n_entries / per_pg.to_f).ceil
     return per_pg, n_pgs
+  end
+  
+  def mangle_unmangle(string)
+    len = string.length
+    last = len - 1
+    middle = len / 2
+
+    [*0..middle].each do |j|
+      k = string[j]
+      string[j] = string[last - j]
+      string[last - j] = k
+    end
+    return string
+  end
+
+  def encrypt(number, width)
+    # returned string would be of length = 'width'
+    return if (width != 7 && width != 3)
+     
+    # Step 1: Subtract given from a constant to get a new number 
+    number = (width == 7) ? (BIG_7 - number) : (BIG_3 - number)
+    m = number.to_s(36).upcase
+     
+    # Step 2: Pad - as needed 
+    padding = width - m.length
+    m = padding > 0 ? ('0' * padding + m) : m
+    
+    # Step 3: Mangle - as needed
+    m = mangle_unmangle m
+    return m
+  end
+
+  def decrypt(string)
+    m = mangle_unmangle string
+    len = m.length
+    number = m.to_i(36)
+    number = len == 7 ? (BIG_7 - number) : (BIG_3 - number)
+    return number
   end
 
 end
