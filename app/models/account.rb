9774cf4f80b800a5fffb859106e02440c5f21cce
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2013-04-20 02:04:57 +0530

    Leave password related validations to Devise's validatable model ...
    Reset min. password length requirement to 6

--- app/models/account.rb
+++ app/models/account.rb
@@ -34,8 +34,6 @@ class Account < ActiveRecord::Base
 
   validates :email, :presence => true
   validates :email, :uniqueness => true
-  validates :password, :presence => true
-  validates :password, :length => { :minimum => 6 }
 
   # An account can be for a student, parent, teacher, school etc. 
   # Hence, set up a polymorphic association 
