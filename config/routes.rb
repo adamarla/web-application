0467cee758a4ccf9303daa69a91a5276eafc6ee4
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2013-06-03 16:08:31 +0530

    [#82] Last bits of resolution panel added ...

--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,7 @@ Webapp::Application.routes.draw do
   match 'restore_scan' => 'examiners#restore_pristine_scan', :via => :get
   match 'pages/unresolved' => 'examiners#unresolved_scans', :via => :get
   match 'unresolved/preview' => 'examiners#preview_unresolved', :via => :get
-  match 'resolve' => 'examiners#resolve_scan', :via => :get
+  match 'resolve' => 'examiners#resolve_scan', :via => :post
   match 'update_scan_id' => 'examiners#update_scan_id', :via => :get
   match 'update_scan_id' => 'examiners#update_scan_id', :via => :post
 
