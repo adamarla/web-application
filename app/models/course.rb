3311b56db30e714977bc9fc133ed2a028d5c4810
Abhinav Chaturvedi <abhinav@gradians.com>
2013-08-13 12:27:07 +0530

    Two new query methods - available_[lessons | quizzes] - added ...
    These return the lessons and quizzes that aren't yet part of
    the given course

--- app/models/course.rb
+++ app/models/course.rb
@@ -15,4 +15,27 @@ class Course < ActiveRecord::Base
 
   belongs_to :teacher
   has_many :milestones
+
+  def available_quizzes
+=begin
+  Returns list of quizzes: 
+    1. Made by the instructor of this course 
+    2. and which are NOT being used in this course
+=end
+    all = Quiz.where(teacher_id: self.teacher_id).map(&:id)
+    used = Coursework.where(milestone_id: self.milestone_ids).map(&:quiz_id)
+    return Quiz.where(id: (all - used))
+  end
+
+  def available_lessons
+=begin
+  Returns list of lessons: 
+    1. Made by any instructor - and not just the one for the course 
+    2. and which are NOT being used in this course
+=end
+    all = Lecture.all.map(&:lesson_id)
+    used = Lecture.where(milestone_id: self.milestone_ids).map(&:lesson_id)
+    return Lesson.where(id: (all - used))
+  end
+
 end
