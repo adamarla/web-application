938a0841ab1493f99bbc83af6ca85855be9f8d21
Abhinav Chaturvedi <abhinav@gradians.com>
2013-09-02 18:57:59 +0530

    Minor stylistic change

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -115,8 +115,7 @@ module ApplicationHelper
       rel = title = placement = nil
     end
 
-    content_tag :button, :class => "btn #{klass}", :type => type, :id => id, 
-      :rel => rel, :title => title, :data => options do 
+    content_tag :button, class: "btn #{klass}", type: type, id: id, rel: rel, title: title, data: options do 
       render = label.blank? ? "" : label 
       render += (icon.blank? ? "" : content_tag(:i, nil, :class => "icon-white #{icon}") )
       unless as.blank?
