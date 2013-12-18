01f074efde16b6f0ee382b0c3c7b1ae52d688c64
Abhinav Chaturvedi <abhinav@gradians.com>
2013-12-18 18:01:59 +0530

    New custom delayed job - DispatchMail - added ...
    Defining a custom job - as opposed to calling .delay - allows us
    to override some of the defaults - like # of re-attempts. Ordinarily, we do
    not re-attempts ( what's the point ? ). But with e-mails, there is a case
    for re-attempting

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -114,7 +114,8 @@ class QuestionController < ApplicationController
       if (@gating.count > 0 || @non_gating.count > 0 || !@comments.blank?)
         @author = Examiner.find @question.examiner_id
         @author = @author.account.active ? @author : Examiner.available.sample(1).first
-        Mailbot.delay.send_audit_report(@question, @author, @gating, @non_gating, @comments)
+        mail = Mailbot.send_audit_report(@question, @author, @gating, @non_gating, @comments)
+        Delayed::Job.enqueue DispatchMail.new(mail), priority: 10
       end
       render json: { msg: "Audit Report Sent", disabled: [@question.id] }, status: :ok
     else
