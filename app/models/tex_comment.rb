b8e2615d2754d01a19875f0c8849fab733990b7e
Abhinav Chaturvedi <abhinav@gradians.com>
2014-02-03 16:25:52 +0530

    First version of heuristic to ascertain whether or not a comment is trivial

--- app/models/tex_comment.rb
+++ app/models/tex_comment.rb
@@ -16,4 +16,24 @@ class TexComment < ActiveRecord::Base
   has_many :remarks
 
   validates :text, uniqueness: true
+
+  def trivial?
+    return self.trivial unless self.trivial.nil?
+    c = crux
+    txt = c.select{ |j| j =~ /^text/ }
+    math = (c - txt).select{ |m| !(m =~ /surd|times|\?/) } # ignore standalone $\surd$, $\times$ and $?$ as math
+    n_words = txt.blank? ? 0 : txt.map{ |m| m.split.count }.inject(:+) # number of english words
+    trivial =  n_words < 7 && math.count < 1
+    self.update_attribute :trivial, trivial
+    return trivial
+  end
+
+  private
+      def crux 
+        # Returns the array of non-trivial tokens in a comment 
+        # Trivial tokens include: empty \text{ }, \*arrow and empty strings
+        a = self.text.split("\\")
+        return a.select{ |j| !j.blank? && !(j =~ /arrow/) && !(j =~ /^text{ }/) } 
+      end
+
 end
