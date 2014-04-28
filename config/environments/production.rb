42c81f1d87e76ad6ef8ec4bd92d0b371c4c7d402
Akshay Damarla <akshay.damarla@gmail.com>
2014-04-28 23:08:55 +0530

    included customer.js in precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Webapp::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true 
 
-  config.assets.precompile += ['admin.js', 'teachers.js', 'students.js', 'welcome.js', 'external.js', 'faq.js']
+  config.assets.precompile += ['admin.js', 'teachers.js', 'students.js', 'welcome.js', 'external.js', 'faq.js', 'customer.js']
   config.assets.js_compressor = :uglifier
 
   # Railscasts #146
