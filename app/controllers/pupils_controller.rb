c0f4ee4508a49863575197b05a6b99c8bd7611ad
Abhinav Chaturvedi <abhinav@gradians.com>
2015-06-14 17:44:28 +0530

    Controller action for creating a new pupil and/or ...
    ... returning the database ID if record already exists

--- app/controllers/pupils_controller.rb
+++ app/controllers/pupils_controller.rb
@@ -2,7 +2,17 @@ class PupilsController < ApplicationController
   respond_to :json
 
   def create_or_update 
-    render json: { status: :ok }, status: :ok
-  end 
+    pupil = Pupil.where(email: params[:email]).first 
 
+    if pupil.nil?
+      pupil = Pupil.new first_name: params[:first_name], 
+                      last_name: params[:last_name], 
+                      email: params[:email],
+                      gender: params[:gender]
+      id = pupil.save ? pupil.id : 0 
+      render json: { id: id }, status: :ok 
+    else
+      render json: { id: pupil.id }, status: :ok
+    end 
+  end 
 end
