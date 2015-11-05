ecdab4f58a65a59b64022b112deb3fb7ff9ee829
Abhinav Chaturvedi <abhinav@gradians.com>
2015-11-05 13:27:20 +0530

    Don't issue GCM calls if there are no valid registration ids

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -14,25 +14,28 @@ class DevicesController < ApplicationController
     api_key = "AIzaSyCuk-OPh2qoB4b9mlAYUeLAJdMlVowk2hY" # dev key 
     # api_key = "AIzaSyCFH3hFqMdGP1dyqSkEyZgrpxHJwbKru68" # release key 
 
-    # Pick a question of the day 
-    q = Question.where(potd: true).order(:num_potd).first 
-    b = BundleQuestion.where(question_id: q.id).first 
-    q.update_attribute(:num_potd, q.num_potd + 1)
-
-    # Send GCM call 
-
-    gcm = GCM.new(api_key)
+    # Ensure there is someone to send notifications to
     reg_ids = Device.where(live: true).map(&:gcm_token)
-    payload = {
-      collapse_key: 'potd', 
-      time_to_live: 86400, # a single day
-      data: { packet: { label: b.name, uid: q.uid, id: q.id } }
-    }
-    response = gcm.send reg_ids, payload 
 
-    # Any tokens the GCM server says are invalid should be invalidated here too.
-    unless response[:not_registered_ids].blank?
-      Device.where(gcm_token: response[:not_registered_ids]).map(&:invalidate)
+    unless reg_ids.blank?
+      # Pick a question of the day 
+      q = Question.where(potd: true).order(:num_potd).first 
+      b = BundleQuestion.where(question_id: q.id).first 
+      q.update_attribute(:num_potd, q.num_potd + 1)
+
+      # Send GCM call 
+      gcm = GCM.new(api_key)
+      payload = {
+        collapse_key: 'potd', 
+        time_to_live: 86400, # a single day
+        data: { packet: { label: b.name, uid: q.uid, id: q.id } }
+      }
+      response = gcm.send reg_ids, payload 
+
+      # Any tokens the GCM server says are invalid should be invalidated here too.
+      unless response[:not_registered_ids].blank?
+        Device.where(gcm_token: response[:not_registered_ids]).map(&:invalidate)
+      end 
     end 
     render nothing: true, status: :ok
   end 
