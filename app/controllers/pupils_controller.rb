c2a2ee8147b7bb75a7cbf55830a36200fe8e786d
Abhinav Chaturvedi <abhinav@gradians.com>
2015-12-11 11:04:30 +0530

    Pupils#ping should also just update the GCM token (if so required)

--- app/controllers/pupils_controller.rb
+++ app/controllers/pupils_controller.rb
@@ -9,10 +9,20 @@ class PupilsController < ApplicationController
                       last_name: params[:last_name], 
                       email: params[:email],
                       gender: params[:gender]
-      id = pupil.save ? pupil.id : 0 
-      render json: { id: id }, status: :ok 
+      pid = pupil.save ? pupil.id : 0 
     else
-      render json: { id: pupil.id }, status: :ok
+      pid = pupil.id 
     end 
+
+    unless params[:id].blank? # updating something else of a previously created record
+      unless params[:gcm_token].blank? 
+        device = Device.where(pupil_id: params[:id], gcm_token: params[:gcm_token]).first 
+        device = Device.create(pupil_id: params[:id], gcm_token: params[:gcm_token]) if device.nil?
+      else 
+        device = nil 
+      end 
+      pid = 0 if device.nil? 
+    end 
+    render json: { id: pid }, status: (pid > 0 ? :ok : :internal_server_error)
   end 
 end
