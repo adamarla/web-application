258b0cb4b71ce794b7ce4c18537b61193b26a5de
Abhinav Chaturvedi <abhinav@gradians.com>
2015-12-15 23:43:37 +0530

    Another silly syntax error corrected

--- app/controllers/daily_streaks_controller.rb
+++ app/controllers/daily_streaks_controller.rb
@@ -7,7 +7,7 @@ class DailyStreaksController < ApplicationController
                                streak_total: params[:total])
 
     saved = streak.new_record? ? 
-      streak.save : streak.update_attribute(streak_total: params[:total])
+      streak.save : streak.update_attribute(:streak_total, params[:total])
 
     render json: { updated: true }, status: (saved ? :ok : :internal_server_error)
   end 
