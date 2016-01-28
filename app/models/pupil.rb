79177e099117130824a58a77fa3e1619c8e587af
Abhinav Chaturvedi <abhinav@gradians.com>
2016-01-28 13:36:57 +0530

    minor edit to returning_user? method

--- app/models/pupil.rb
+++ app/models/pupil.rb
@@ -41,8 +41,11 @@ class Pupil < ActiveRecord::Base
   def returning_user? 
     a = Attempt.where(pupil_id: self.id) 
     return false if a.empty? 
-    dates = a.map{ |x| x.updated_at.to_date.strftime("%d / %m / %y") }
-    return dates.uniq.count > 1 
+    format = "%d/%m/%y"
+    dates = a.map{ |x| x.updated_at.to_date.strftime(format) }
+    return true if dates.uniq.count > 1 
+    signed_up_on = self.created_at.to_date.strftime(format)
+    return signed_up_on != dates.first
   end 
 
 
