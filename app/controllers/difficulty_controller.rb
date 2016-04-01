02219a41a71bc24eed04a46b7b579960b24cdb97
Abhinav Chaturvedi <abhinav@gradians.com>
2016-04-01 17:30:44 +0530

    Minor edits to list action

--- app/controllers/difficulty_controller.rb
+++ app/controllers/difficulty_controller.rb
@@ -3,7 +3,7 @@ class DifficultyController < ApplicationController
   respond_to :json 
 
   def list 
-    response = Difficulty.order(:level).map{ |d| { id: d.id, name: d.name } }
+    response = Difficulty.order(:level).map{ |d| { id: d.id, name: "#{d.name.titleize} (#{d.meaning})" } }
     render json: response, status: :ok 
   end 
 
