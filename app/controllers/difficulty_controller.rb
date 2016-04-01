722f343861e16f78789aed356ec94220988d151f
Abhinav Chaturvedi <abhinav@gradians.com>
2016-04-01 08:20:44 +0530

    Return the DB id so as to be consistent with similar actions

--- app/controllers/difficulty_controller.rb
+++ app/controllers/difficulty_controller.rb
@@ -3,7 +3,7 @@ class DifficultyController < ApplicationController
   respond_to :json 
 
   def list 
-    response = Difficulty.order(:level).map{ |d| { id: d.level, name: d.name } }
+    response = Difficulty.order(:level).map{ |d| { id: d.id, name: d.name } }
     render json: response, status: :ok 
   end 
 
