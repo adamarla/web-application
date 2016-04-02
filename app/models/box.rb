51797f29521b15a80afdeec3fca4fba89e6226c8
Abhinav Chaturvedi <abhinav@gradians.com>
2016-04-02 11:59:25 +0530

    Tightened validations and call backs
    A box created for questions ensures that there are boxes for
    snippets and skills to go with it.

--- app/models/box.rb
+++ app/models/box.rb
@@ -16,18 +16,51 @@
 #
 
 class Box < ActiveRecord::Base
+  validates :name, uniqueness: true 
   validates :chapter_id, numericality: { only_integer: true, greater_than: 0 }
-  validates :chapter_id, uniqueness: { scope: [:language_id, :min_difficulty, :max_difficulty] } 
+  validates :chapter_id, uniqueness: { scope: [:language_id, :min_difficulty, :max_difficulty] }, if: :of_questions
+  validates :chapter_id, uniqueness: { scope: [:of_snippets, :of_skills] }, unless: :of_questions
 
   belongs_to :chapter 
   belongs_to :language 
 
-  after_create :set_name 
+  after_create :seal 
+
+  def set_difficulty_range(min, max) 
+    return if (min == 0 || max == 0) 
+    if (min > max) 
+      tmp = min 
+      min = max 
+      max = tmp 
+    end 
+
+    # Set the difficulties
+    self.update_attributes min_difficulty: min, max_difficulty: max
+  end 
+
+
+  def self.for_chapter(chapter, language = Language.named('english'))
+    # There can be multiple box of questions for the same chapter. 
+    # The difference could be in target language, difficulty levels etc. 
+    # But there can be *only one* box of skills and snippets. 
+    
+    # This method creates a box for questions - and any for snippets
+    # and skills.
+
+    b = Box.create(chapter_id: chapter, language_id: language, of_questions: true)
+    b.set_difficulty_range Difficulty.named('easy'), Difficulty.named('medium')
+  end 
 
   private 
-    def set_name 
+    def seal 
       hex_time = Time.now.to_i.to_s(16)
-      self.update_attribute :name, hex_time
+      prefix = self.of_questions ? "q" : (self.of_skills ? "sk" : "sn")
+      self.update_attribute :name, "#{prefix}-#{hex_time}"
+
+      return unless self.of_questions
+      Box.create(chapter_id: self.chapter_id, of_skills: true) 
+      Box.create(chapter_id: self.chapter_id, of_snippets: true) 
+
     end 
 
 end
