aa2e94a4fad2a6cade709ba43ff49b54fd8eba25
Abhinav Chaturvedi <abhinav@gradians.com>
2016-04-04 16:24:54 +0530

    If a skill's chapter is changed, then it's time to repackage it.

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -9,11 +9,16 @@
 
 class Skill < ActiveRecord::Base
   has_one :sku, as: :stockable, dependent: :destroy
+  before_update :set_sku_modified, if: :chapter_id_changed?
   after_create :add_sku 
 
   private 
     def add_sku 
       self.create_sku path: "skill/#{self.id}"
+    end 
+
+    def set_sku_modified 
+      self.sku.update_attribute :modified, true 
     end 
 
 end
