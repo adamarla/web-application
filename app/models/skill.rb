7eab8bd174f7a8863891fe43fcc9780e112ac36a
Abhinav Chaturvedi <abhinav@gradians.com>
2016-04-07 23:20:09 +0530

    Change the generic column if Skill assigned to a different chapter

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -12,20 +12,18 @@ class Skill < ActiveRecord::Base
   belongs_to :chapter 
   has_one :sku, as: :stockable, dependent: :destroy
 
+  after_create :seal
   around_update :set_sku_ownership, if: :chapter_id_changed?
-  after_create :add_sku, :set_uid 
 
   private 
-    def add_sku 
+    def seal 
       self.create_sku path: "skills/#{self.id}"
-    end 
-
-    def set_uid 
-      self.update_attribute :uid, "#{self.chapter.uid}-#{self.id}"    
+      self.update_attributes uid: "#{self.chapter.uid}-#{self.id}", generic: (self.chapter_id == Chapter.generic.id)
     end 
 
     def set_sku_ownership 
-      yield
+      self.generic = self.chapter_id == Chapter.generic.id 
+      yield 
       self.sku.edit_zips
     end 
 
