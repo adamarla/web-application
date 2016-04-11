4d4a40dcb93d5873320c91582d6eb19c25256179
Abhinav Chaturvedi <abhinav@gradians.com>
2016-04-11 15:45:12 +0530

    chapter/list adapted to process string arguments passed by bash script 'syllabus'

--- app/controllers/chapter_controller.rb
+++ app/controllers/chapter_controller.rb
@@ -3,14 +3,20 @@ class ChapterController < ApplicationController
   respond_to :json 
 
   def list 
-    level = params[:level] || Level.named('senior') 
-    subject = params[:subject] || Subject.named('maths') 
+    if params[:cmdline]
+      level = Level.named params[:level]
+      subject = Subject.named params[:subject]
+    else 
+      level = params[:level] || Level.named('senior') 
+      subject = params[:subject] || Subject.named('maths') 
+    end 
+
     list = Chapter.where(level_id: level, subject_id: subject).order(:name)
     response = list.blank? ? [] : list.map{ |s| { id: s.id, name: s.name } } 
     render json: response, status: :ok
   end 
 
-  # To be called ** only ** by bash script 
+  # To be called ** only ** by the bash script called 'syllabus'
   def parcels
     parcels = Parcel.where(chapter_id: params[:c]) 
     render json: parcels.map(&:to_json), status: :ok
