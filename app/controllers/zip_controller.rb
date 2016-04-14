29fe83a8ccd98e2eabe0a5a9cb595042bc7c5724
Abhinav Chaturvedi <abhinav@gradians.com>
2016-04-14 22:57:59 +0530

    (zip/contents) Return the chapter_id to which the zip belongs ...
    The chapter_id should go in the zip's catalog.
    Its the only way to search for SKUs by chapter on the phone.

--- app/controllers/zip_controller.rb
+++ app/controllers/zip_controller.rb
@@ -2,6 +2,7 @@
 class ZipController < ApplicationController
   respond_to :json 
 
+  # Only from zippify 
   def update
     # Once the zip has been re-generated, update its SHASUM 
     zip = Zip.find params[:id] 
@@ -10,18 +11,23 @@ class ZipController < ApplicationController
     render json: { id: params[:id] }, status: :ok
   end 
 
+  # Only from zippify 
+  def list_contents
+    zip = Zip.find params[:id]
+    skus = zip.skus 
+    render json: { 
+                    name: zip.name, 
+                    id: zip.id, 
+                    chapter: zip.parcel.chapter_id, 
+                    skus: skus.map{ |s| { id: s.id, type: s.stockable_type, path: s.path } } 
+                 }, status: :ok 
+  end 
+
+  # From the mobile app 
   def ping 
     zip = Zip.find params[:id] 
     render json: { id: zip.id, shasum: zip.shasum }, status: :ok
   end 
 
-  def list_contents
-    zip = Zip.find params[:id]
-    skus = zip.skus 
-    render json: { 
-                    name: zip.name, id: zip.id, 
-                    skus: skus.map{ |s| { id: s.id, type: s.stockable_type, path: s.path } } 
-                 }, status: :ok 
-  end 
 
 end # of class
