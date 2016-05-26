fadb811ecb495d028c1b970a9aaed985c84f2a12
Abhinav Chaturvedi <abhinav@gradians.com>
2016-05-26 09:07:50 +0530

    Removed tag/question

--- config/routes.rb
+++ config/routes.rb
@@ -149,7 +149,6 @@ Webapp::Application.routes.draw do
 
     match 'question/set_potd_flag' => 'question#set_potd_flag', via: :get
     match 'bundle/which' => 'question#bundle_which', via: :get
-    match 'tag/question' => 'question#tag', via: :post
     match 'q/loc' => 'question#new_location', via: :get
 
     # Videos 
