6addc1ff07427df6b96fb4452af7c825c5ad86db
Abhinav Chaturvedi <abhinav@gradians.com>
2016-08-31 00:49:58 +0530

    Query to search for usages by app version added
    Only usages by newcomers are queried.
    
    If filtering by app version is important, then best to
    start the composite query with on_app_version

--- app/models/usage.rb
+++ app/models/usage.rb
@@ -30,41 +30,47 @@ class Usage < ActiveRecord::Base
     where('user_id > ?', 537).order(:id)
   end 
 
+  def self.on_app_version(min_version = 1) 
+    return self.newcomers if (min_version < 2) 
+    uids = User.where('app_version IS NOT ?', nil).where('app_version >= ?', min_version.to_s).map(&:id) 
+    return self.newcomers.where(user_id: uids)
+  end 
+
   def self.questions_done(min = 1) 
-    self.newcomers.where('num_questions_done >= ?', min) 
+    self.where('num_questions_done >= ?', min) 
   end 
 
   def self.snippets_done(min = 1)
-    self.newcomers.where('num_snippets_done >= ?', min) 
+    self.where('num_snippets_done >= ?', min) 
   end 
 
   def self.something_done 
-    self.newcomers.where('num_snippets_done > ? OR num_questions_done > ?', 0,0)
+    self.where('num_snippets_done > ? OR num_questions_done > ?', 0,0)
   end 
 
   def self.something_clicked
-    self.newcomers.where('num_snippets_clicked > ? OR num_questions_clicked > ?', 0,0)
+    self.where('num_snippets_clicked > ? OR num_questions_clicked > ?', 0,0)
   end 
 
   def self.seen_stats
-    self.newcomers.where('time_on_stats > ?', 0)
+    self.where('time_on_stats > ?', 0)
   end 
 
   def self.some_time_spent
-    self.newcomers.where('time_on_snippets > ? OR time_on_questions > ? OR time_on_stats > ?', 0,0,0)
+    self.where('time_on_snippets > ? OR time_on_questions > ? OR time_on_stats > ?', 0,0,0)
   end 
 
   def self.probability_questions_y_given_x(x = 1,y = 1) 
     return 0 if (x < 1 || y < 1) 
     return 100 if (y <= x) 
 
-    num_y = self.questions_done(y).map(&:user_id).uniq.count 
-    num_x = self.questions_done(x).map(&:user_id).uniq.count 
+    num_y = self.questions_done(y).map(&:user_id).uniq.count
+    num_x = self.questions_done(x).map(&:user_id).uniq.count
 
     return ((num_y.to_f / num_x) * 100).round(2) 
   end 
 
-  def self.probability_snippets_y_given_x(x = 1,y = 1) 
+  def self.probability_snippets_y_given_x(x = 1,y = 1, post_version_two = false) 
     return 0 if (x < 1 || y < 1) 
     return 100 if (y <= x) 
 
