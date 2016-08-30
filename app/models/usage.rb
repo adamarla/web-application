a1aea47995bc71f02541c4a1f3dca0d99d4dccc9
Abhinav Chaturvedi <abhinav@gradians.com>
2016-08-30 23:58:18 +0530

    Added some new query methods

--- app/models/usage.rb
+++ app/models/usage.rb
@@ -30,12 +30,24 @@ class Usage < ActiveRecord::Base
     where('user_id > ?', 537).order(:id)
   end 
 
+  def self.questions_done(min = 1) 
+    self.newcomers.where('num_questions_done >= ?', min) 
+  end 
+
+  def self.snippets_done(min = 1)
+    self.newcomers.where('num_snippets_done >= ?', min) 
+  end 
+
   def self.something_done 
     self.newcomers.where('num_snippets_done > ? OR num_questions_done > ?', 0,0)
   end 
 
   def self.something_clicked
     self.newcomers.where('num_snippets_clicked > ? OR num_questions_clicked > ?', 0,0)
+  end 
+
+  def self.seen_stats
+    self.newcomers.where('time_on_stats > ?', 0)
   end 
 
   def self.some_time_spent
