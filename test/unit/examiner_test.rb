8ad80e98b71a2d8f44d43cb16a6b714cbd08d378
Abhinav Chaturvedi <abhinav@gradians.com>
2017-04-10 13:19:17 +0530

    Removed 37 (previously trashed) models and their associated tables.

--- test/unit/examiner_test.rb
+++ test/unit/examiner_test.rb
@@ -2,19 +2,13 @@
 #
 # Table name: examiners
 #
-#  id                :integer         not null, primary key
-#  created_at        :datetime
-#  updated_at        :datetime
-#  is_admin          :boolean         default(FALSE)
-#  first_name        :string(30)
-#  last_name         :string(30)
-#  last_workset_on   :datetime
-#  n_assigned        :integer         default(0)
-#  n_graded          :integer         default(0)
-#  live              :boolean         default(FALSE)
-#  mentor_id         :integer
-#  mentor_is_teacher :boolean         default(FALSE)
-#  internal          :boolean         default(FALSE)
+#  id         :integer         not null, primary key
+#  created_at :datetime
+#  updated_at :datetime
+#  is_admin   :boolean         default(FALSE)
+#  first_name :string(30)
+#  last_name  :string(30)
+#  live       :boolean         default(FALSE)
 #
 
 require 'test_helper'
