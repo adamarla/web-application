cc868abff85685e4145c9ec4458336756ad8cc7e
Akshay Damarla <akshay.damarla@gmail.com>
2017-05-22 20:40:20 -0500

    selection logic fixed

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -4,7 +4,7 @@ class ActivityController < ApplicationController
         
 
     sku_type = params[:sku_type] == 1 ? "Question" : "Snippet"
-    sku_id = Sku.where(stockable_id: params[:id], stockable_type: sku_type).first.id
+    sku_id = Sku.where(stockable_id: params[:id]).select{|sku| sku.stockable.type == sku_type}
 
     activity = Activity.where(user_id: params[:uid], sku_id: sku_id).first ||
         Activity.create(user_id: params[:uid], sku_id: sku_id, num_attempts: 0)
